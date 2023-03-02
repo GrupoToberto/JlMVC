5ea09216e00af1e16b90c40c67be0ee8022d7503
GrupoToberto <103981508+GrupoToberto@users.noreply.github.com>
2023-03-02 02:40:03 -0600

    Add files via upload

---
+++ controllers/Controller.php
@@ -0,0 +1,91 @@
+<?php
+    namespace com\grupotoberto\jlmvc\controllers;
+
+    class Controller{
+        
+        protected static function View(){
+            $Aux=explode("\\", debug_backtrace()[1]['class']);
+            $viewGroup=str_replace("Controller", "", $Aux[count($Aux)-1]);
+            $view=debug_backtrace()[1]['function'];
+            $uri=$_SERVER['DOCUMENT_ROOT']."/views/".$viewGroup."/".$view.".html";
+
+            $file=fopen($uri, "r");
+            $template=fread($file,filesize($uri));
+            fclose($file);
+
+            return self::fixTemplate($template);
+        }
+
+        private static function fixTemplate($template){
+
+            try{
+                if(substr_count($template, "@extends")==1){
+                    $pos=strpos($template, '@extends'); 
+                    $layout=substr($template, $pos+10); 
+                    $posf=strpos($layout, "')");
+                    $layout=substr($template, $pos+10, $posf); 
+
+                    $uri=$_SERVER['DOCUMENT_ROOT']."/views/".$layout.".html";
+
+                    $file=fopen($uri, "r");
+                    $fTemplate=fread($file,filesize($uri));
+                    fclose($file);
+;
+                    $template=str_replace("@extends('".$layout."')", $fTemplate, $template);
+                    
+                    $template=self::fixSections($template);
+                }
+            }
+            catch(Exception $e){
+
+            }
+
+            return $template;
+        }
+
+        private static function fixSections($template){
+            $nSections=substr_count($template, "@section");
+            $Sections=array();
+            $Inners=array();
+            $i=$nSections;
+
+            try{
+                while($i>0){
+                    $pos=strpos($template, '@section'); 
+                    $Sections[$i]=substr($template, $pos+10); 
+                    $posf=strpos($Sections[$i], "')");
+                    $Sections[$i]=substr($template, $pos+10, $posf); 
+    
+                    $posInner=strpos($template, "@section('".$Sections[$i]."')");
+                    $Inners[$i]=substr($template, $posInner+strlen("@section('".$Sections[$i]."')")); 
+                    $posfInner=strpos($Inners[$i], "@endsection");
+                    $Inners[$i]=substr($template, $posInner+strlen("@section('".$Sections[$i]."')"), $posfInner); 
+    
+                    $template=str_replace("@section('".$Sections[$i]."')", "", $template);
+                    $template=str_replace($Inners[$i]."@endsection", "", $template);
+    
+                    $i--;
+                } 
+                $template=self::fixContents($template, $Sections, $Inners);
+            }
+            catch(Exception $e){
+
+            }
+
+            return $template;
+        }
+
+        private static function fixContents($template, $Sections, $Inners){
+
+            try{
+                for($i=1; $i<=count($Sections); $i++)
+                    $template=str_replace("@content('".$Sections[$i]."')", $Inners[$i], $template);
+            }
+            catch(Exception $e){
+
+            }
+
+            return $template;
+        }
+
+    }
---
+++ routes/router.php
@@ -0,0 +1,32 @@
+<?php
+    namespace com\grupotoberto\jlmvc;
+    use com\grupotoberto\jlweb\WebManager as WM;
+    class Router{
+        private $controller;
+        private $method;
+
+        public function __construct(){
+            $this->matchRoute();
+        }
+
+        public function matchRoute(){
+            WM::setResFolder("/res/");
+
+            $url = explode('/', URL);
+            $DEFAULT_CONTROLLER=WM::getString("DEFAULT_CONTROLLER")."";
+            $DEFAULT_METHOD=WM::getString("DEFAULT_METHOD")."";
+
+            $this->controller=!empty($url[0])?$url[0].'Controller':$DEFAULT_CONTROLLER;
+            $this->method=!empty($url[1])?$url[1]:$DEFAULT_METHOD;
+
+            require_once($_SERVER['DOCUMENT_ROOT']."/controllers/".$this->controller.".php");
+
+            $this->controller=WM::getString("mainNamespace")."\\".$this->controller;
+        }
+
+        public function run(){
+            $controller = new $this->controller();
+            $method = $this->method;
+            $controller->$method();
+        }
+    }
